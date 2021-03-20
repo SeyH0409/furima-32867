652e2158416fae44621e2755dcd521950a603423
SeyH0409 <199894sei@gmail.com>
2021-03-20 17:13:02 +0900

    商品出品機能の実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,20 +1,24 @@
 class Product < ApplicationRecord
   with_options presence: true do
+    validates :image
     validates :name
     validates :description
-    validates :price
-    validates :quality_id
-    validates :category_id
-    validates :delivery_fee_id
-    validates :delivery_day_id
-    validates :prefecture_id
+    validates :price, format: { with: /\A[0-9]+\z/ }, 
+                      numericality: {greater_than: 300, less_than: 9999999}
+    with_options numericality: { other_than: 1} do
+      validates :quality_id
+      validates :category_id
+      validates :delivery_fee_id
+      validates :delivery_day_id
+      validates :prefecture_id
+    end
   end
 
   belongs_to :user
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :quality
-  belongs_to :categories
+  belongs_to :category
   belongs_to :delivery_fee
   belongs_to :delivery_day
   belongs_to :prefecture
