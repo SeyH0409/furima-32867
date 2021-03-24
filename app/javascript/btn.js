6d943cbeb7e9ed12aad784d6b4c4e39ff04dcef7
SeyH0409 <199894sei@gmail.com>
2021-03-24 13:37:50 +0900

    詳細画面　修正

--- app/javascript/btn.js
+++ app/javascript/btn.js
@@ -1,16 +1,9 @@
 window.addEventListener('load', () => {
-  const editBtn = document.getElementById('edit-btn');
-  const buyBtn  = document.getElementById('buy-btn');
-  editBtn.addEventListener('mouseover', () => {
-    editBtn.setAttribute('style', 'background-color:#FFBEDA;');
+  const btn = document.getElementById('btn');
+  btn.addEventListener('mouseover', () => {
+    btn.setAttribute('style', 'background-color:#FFBEDA;');
   });
-  editBtn.addEventListener('mouseout', () => {
-    editBtn.removeAttribute('style', 'background-color:#FFBEDA;');
-  });
-  buyBtn.addEventListener('mouseover', () => {
-    buyBtn.setAttribute('style', 'background-color:#FFBEDA;');
-  });
-  buyBtn.addEventListener('mouseout', () => {
-    buyBtn.removeAttribute('style', 'background-color:#FFBEDA;');
+  btn.addEventListener('mouseout', () => {
+    btn.removeAttribute('style', 'background-color:#FFBEDA;');
   });
 });
