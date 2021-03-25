16dbd3b98d3a00458f76c7df4f61bc3fe82c225e
SeyH0409 <199894sei@gmail.com>
2021-03-25 11:52:53 +0900

    商品情報編集機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,6 +21,23 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+    unless current_user.id == @product.user_id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    if @product.save
+      redirect_to product_path(@product.id)
+    else
+      render :edit
+    end
+  end
+
   private
   def product_params
     params.require(:product).permit(:name, :description, :price, :quality_id, :category_id, 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: "products#index"
   devise_for :users
-  resources :products, only: [:new, :create, :index, :show]
+  resources :products, only: [:new, :create, :index, :show, :edit, :update]
 end
