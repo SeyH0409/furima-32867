d7c8a3181eb1d8dbacafd5a1a76ae987ba6f3ccb
SeyH0409 <199894sei@gmail.com>
2021-03-25 15:52:10 +0900

    編集機能　修正　＃１

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_product, only: [:show, :edit, :update]
+
   def index
     @products = Product.all.order("created_at DESC")
   end
@@ -18,23 +20,22 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
     unless current_user.id == @product.user_id
       redirect_to root_path
     end
   end
 
   def update
-    @product = Product.find(params[:id])
-    @product.update(product_params)
-    if @product.save
+    if @product.update(product_params)
       redirect_to product_path(@product.id)
     else
       render :edit
+    end
+    unless current_user.id == @product.user_id
+      redirect_to root_path
     end
   end
 
@@ -42,5 +43,9 @@ class ProductsController < ApplicationController
   def product_params
     params.require(:product).permit(:name, :description, :price, :quality_id, :category_id, 
                                     :delivery_fee_id, :delivery_day_id, :prefecture_id, :image).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
