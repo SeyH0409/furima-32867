8a272115fb740a7d0250f83390c357134ed15d9c
SeyH0409 <199894sei@gmail.com>
2021-04-06 11:15:23 +0900

    商品購入機能の実装

---
+++ app/models/order_address.rb
@@ -0,0 +1,21 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :prefecture_id, :postcode, :city, :block, :building, :phone_number, 
+                :user_id, :product_id, 
+                :token
+  
+  with_options presence: true do
+    validates :postcode format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :city 
+    validates :block
+    validates :phone_number format: { with: /\A\d{10,11}\z/ }
+    validates :token
+    validates :prefecture_id, numericality: { other_than: 1 }
+  end
+
+  def save
+    order = Order.create(product_id: product_id, user_id: user_id)
+    Address.create(postcode: postcode, city: city, block: block, 
+                   phone_number: phone_number, prefecture_id: prefecture_id, order_id: order.id )
+  end
+end
